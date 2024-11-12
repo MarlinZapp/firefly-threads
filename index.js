a0616667903630ef94c4c4bcc925a8f7639bce4e
Marlin Zapp <s4mazapp@uni-trier.de>
2024-11-12 15:17:22 +0100

    successfully initialized threads but they deadlock at the moment because channels are always blocked

--- index.js
+++ index.js
@@ -1,46 +1,70 @@
 // Importing WASM as a JS module requires us to call an init function provided by the default export.
 // This is planned to be changed in the future.
-import { default as wasm, setup_fireflies } from "./pkg/firefly_rust_wasm.js";
+import {
+  default as wasm,
+  setup_fireflies,
+  get_grid_row,
+  start,
+} from "./pkg/firefly_rust_wasm.js";
 
 wasm().then((module) => {
   // Define number of rows and columns
   const m = 9; // Number of rows
   const n = 16; // Number of columns
+  let initialized = false;
 
   setup_fireflies(m, n);
 
   // Reference to the grid container
   const gridContainer = document.getElementById("grid");
+  let cells = [[]];
 
-  renderGrid(m, n);
+  initializeGrid(m, n);
+  start();
+
+  // Periodically poll the state and update the grid
+  setInterval(() => renderGrid(m, n), 100); // Update every 100ms
 
   /**
    * Function to render a grid of m x n cells
-   * @param {number} m - Number of rows
-   * @param {number} n - Number of columns
+   * @param {number} mrows - Number of rows
+   * @param {number} ncols - Number of columns
    */
-  function renderGrid(m, n) {
-    if (!gridContainer) {
-      throw new Error("Grid container not found");
-    }
-
-    // Set up the grid structure using CSS Grid properties
-    gridContainer.style.gridTemplateRows = `repeat(${m}, 1fr)`;
-    gridContainer.style.gridTemplateColumns = `repeat(${n}, 1fr)`;
-
-    for (let i = 0; i < m * n; i++) {
-      const cell = document.createElement("div");
-      cell.classList.add("grid-cell");
-      //cell.textContent = String(i + 1); // Optional: Display cell number
-      cell.addEventListener("click", () => changeColor(cell)); // Add click event for color change
-      gridContainer.appendChild(cell);
+  function renderGrid(mrows, ncols) {
+    for (let i = 0; i < mrows; i++) {
+      const row = get_grid_row(i);
+      for (let j = 0; j < ncols; j++) {
+        if (row[j] === 0) {
+          cells[i][j].style.backgroundColor = "black";
+        } else {
+          cells[i][j].style.backgroundColor = "white";
+        }
+      }
     }
   }
 
-  // Function to change cell color when clicked
-  function changeColor(cell) {
-    // Generate a random color
-    const randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-    cell.style.backgroundColor = randomColor;
+  /**
+   * Function to render a grid of m x n cells
+   * @param {number} mrows - Number of rows
+   * @param {number} ncols - Number of columns
+   */
+  function initializeGrid(mrows, ncols) {
+    if (!gridContainer) {
+      throw new Error("Grid container not found");
+    }
+    // Set up the grid structure using CSS Grid properties
+    gridContainer.style.gridTemplateRows = `repeat(${mrows}, 1fr)`;
+    gridContainer.style.gridTemplateColumns = `repeat(${ncols}, 1fr)`;
+
+    for (let i = 0; i < mrows; i++) {
+      cells.push([]);
+      for (let j = 0; j < ncols; j++) {
+        const cell = document.createElement("div");
+        cell.classList.add("grid-cell");
+        cell.id = `cell-${i}-${j}`;
+        gridContainer.appendChild(cell);
+        cells[i].push(cell);
+      }
+    }
   }
 });
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,27 @@
 mod utils;
 
+use js_sys::Math;
 use std::sync::mpsc::{self, Receiver, Sender};
 use std::sync::{Arc, Mutex};
+use std::time::Duration;
 use wasm_bindgen::prelude::*;
 use wasm_thread as thread;
+
+extern crate web_sys;
+
+// A macro to provide `println!(..)`-style syntax for `console.log` logging.
+macro_rules! log {
+    ( $( $t:tt )* ) => {
+        web_sys::console::log_1(&format!( $( $t )* ).into());
+    }
+}
 
 #[wasm_bindgen]
 extern "C" {
     fn alert(s: &str);
 }
+
+static mut GRID_STATE: Option<Vec<Vec<Firefly>>> = None;
 
 #[derive(Clone, Copy, Debug)]
 enum LightState {
@@ -16,104 +29,164 @@ enum LightState {
     Off,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 struct Firefly {
     x: usize,
     y: usize,
-    state: LightState,
-    neighbours: Vec<Arc<Mutex<(Sender<Message>, Receiver<Message>)>>>,
+    state: Arc<Mutex<LightState>>,
     channel: Arc<Mutex<(Sender<Message>, Receiver<Message>)>>,
 }
 
 #[derive(Debug, Clone, Copy)]
 enum Message {
-    TurnOn,
-    TurnOff,
+    Blink,
+}
+
+#[wasm_bindgen]
+pub fn start() {
+    unsafe {
+        if let Some(fireflies) = &GRID_STATE {
+            let (y, x) = (
+                get_random_index(fireflies.len()),
+                get_random_index(fireflies[0].len()),
+            );
+            fireflies[y][x]
+                .channel
+                .lock()
+                .unwrap()
+                .0
+                .send(Message::Blink)
+                .unwrap();
+        }
+    }
+}
+
+fn get_random_index(n: usize) -> usize {
+    // Generates a random floating-point number between 0.0 (inclusive) and 1.0 (exclusive)
+    let random_float = Math::random();
+
+    // Scale to the range [0, n) and convert to u32 for an integer index
+    (random_float * n as f64).floor() as usize
+}
+
+#[wasm_bindgen]
+pub fn get_grid_row(i: usize) -> Vec<u8> {
+    unsafe {
+        if let Some(fireflies) = &GRID_STATE {
+            let mut row_state = Vec::new();
+            let flies = &fireflies[i];
+            for fly in flies {
+                let fly_state = fly.state.lock().unwrap();
+                match *fly_state {
+                    LightState::On => {
+                        row_state.push(1);
+                    }
+                    LightState::Off => {
+                        row_state.push(0);
+                    }
+                }
+            }
+            return row_state;
+        }
+    }
+    vec![]
 }
 
 impl Firefly {
     // Method to create a new firefly
-    fn new(
-        x: usize,
-        y: usize,
-        neighbours: Vec<Arc<Mutex<(Sender<Message>, Receiver<Message>)>>>,
-        channel: Arc<Mutex<(Sender<Message>, Receiver<Message>)>>,
-    ) -> Self {
+    fn new(x: usize, y: usize) -> Self {
+        let channel = mpsc::channel();
+        let channel = Arc::new(Mutex::new(channel));
         let firefly = Firefly {
             x,
             y,
-            state: LightState::Off,
-            neighbours,
+            state: Arc::new(Mutex::new(LightState::Off)),
             channel,
         };
         firefly
     }
 
-    // Method to toggle the light state
-    fn _toggle_light(&mut self) {
-        self.state = match self.state {
-            LightState::On => LightState::Off,
-            LightState::Off => LightState::On,
-        };
-        println!(
-            "Firefly at ({}, {}) is now {:?}",
-            self.x, self.y, self.state
-        );
-    }
-
     // Method to start firefly's behavior in a separate thread
-    fn start(mut self) {
+    fn start(&mut self) {
+        let state = Arc::clone(&self.state);
+        let (x, y) = (self.x.clone(), self.y.clone());
+        let channel = Arc::clone(&self.channel);
         thread::spawn(move || {
-            while let Ok(message) = self.channel.lock().unwrap().1.recv() {
+            while let Ok(message) = channel.lock().unwrap().1.recv() {
+                log!("Test");
                 match message {
-                    Message::TurnOn => self.state = LightState::On,
-                    Message::TurnOff => self.state = LightState::Off,
+                    Message::Blink => {
+                        Firefly::blink(Arc::clone(&state));
+                        Firefly::inform_neighbours(x, y);
+                    }
                 }
-                println!("Firefly at ({}, {}) received {:?}", self.x, self.y, message);
-                self.neighbours.iter().for_each(|neighbor| {
-                    neighbor.lock().unwrap().0.send(message).unwrap();
-                });
+                log!(
+                    "Firefly {},{} is now in state {:?}",
+                    x,
+                    y,
+                    state.lock().unwrap()
+                );
             }
         });
+    }
+
+    fn blink(state: Arc<Mutex<LightState>>) {
+        {
+            let mut state = state.lock().unwrap();
+            *state = LightState::On;
+        }
+        thread::spawn(move || {
+            thread::sleep(Duration::from_secs(1));
+            {
+                let mut state = state.lock().unwrap();
+                *state = LightState::Off;
+            }
+        });
+    }
+
+    fn inform_neighbours(x: usize, y: usize) {
+        unsafe {
+            if let Some(fireflies) = &GRID_STATE {
+                let rows = fireflies.len();
+                let cols = fireflies[0].len();
+                let up = &fireflies[(y - 1) % rows][x];
+                let down = &fireflies[(y + 1) % rows][x];
+                let left = &fireflies[y][(x - 1) % cols];
+                let right = &fireflies[y][(x + 1) % cols];
+                let neighbours = vec![up, down, left, right];
+                for neighbour in neighbours {
+                    neighbour
+                        .channel
+                        .lock()
+                        .unwrap()
+                        .0
+                        .send(Message::Blink)
+                        .unwrap();
+                }
+            }
+        }
     }
 }
 
 #[wasm_bindgen]
-pub fn setup_fireflies(n: usize, m: usize) {
+pub fn setup_fireflies(rows: usize, cols: usize) {
     utils::set_panic_hook();
 
-    let mut channels = Vec::new();
     let mut fireflies = Vec::new();
 
-    // Create the fireflies and store their channels
-    for i in 0..n {
-        channels.push(Vec::new());
-        for _ in 0..m {
-            let (tx, rx) = mpsc::channel();
-            channels[i].push(Arc::new(Mutex::new((tx, rx))));
+    // Connect neighbors with torus-like communication
+    for y in 0..rows {
+        fireflies.push(Vec::new());
+        for x in 0..cols {
+            let mut firefly = Firefly::new(x, y);
+
+            firefly.start();
+
+            fireflies[y].push(firefly);
         }
     }
 
-    // Connect neighbors with torus-like communication
-    for i in 0..n {
-        for j in 0..m {
-            let mut neighbors = vec![];
-            let up = (i + n - 1) % n;
-            let down = (i + 1) % n;
-            let left = (j + m - 1) % m;
-            let right = (j + 1) % m;
-            {
-                neighbors.push(Arc::clone(&channels[up][j]));
-                neighbors.push(Arc::clone(&channels[down][j]));
-                neighbors.push(Arc::clone(&channels[i][left]));
-                neighbors.push(Arc::clone(&channels[i][right]));
-            }
-
-            let firefly = Firefly::new(i, j, neighbors, Arc::clone(&channels[i][j]));
-            fireflies.push(firefly.clone());
-
-            // Start the firefly behavior
-            firefly.start();
-        }
+    unsafe {
+        GRID_STATE = Some(fireflies);
     }
 }
